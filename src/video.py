fff2f1f67969e8879b60255ae6ecd2dadfb5be4c
Sabrina Pryszczuk <sabrinadaianap@gmail.com>
2026-06-13 11:17:26 -0300

    feat: estructura inicial del proyecto y .gitignore

---
+++ src/video.py
@@ -0,0 +1,12 @@
+
+
+class Video:
+    def __init__(self, ruta_archivo):
+        self.path = ruta_archivo
+        self.fotogramas_extraidos = False # atributo para saber el estado
+
+    def convertir_a_fotogramas(self):
+        pass
+
+    
+
